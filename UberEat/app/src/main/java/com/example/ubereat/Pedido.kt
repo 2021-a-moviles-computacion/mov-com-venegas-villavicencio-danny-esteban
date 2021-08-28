2ce83e1b478d23db4d9faa81c3b29788899c5454
nuevo <nuevo>
2021-08-28 15:25:54 -0500

    card view inicio

---
+++ UberEat/app/src/main/java/com/example/ubereat/Pedido.kt
@@ -0,0 +1,10 @@
+package com.example.ubereat
+
+class Pedido(
+    val nombreLocal: String,
+    val costoEnvio: Double,
+    val tiempo: String,
+    val puntuacion: Double
+) {
+
+}
---
+++ UberEat/app/src/main/java/com/example/ubereat/adapters/InicioAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.ubereat.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ubereat.Pedido
+import com.example.ubereat.R
+
+class InicioAdapter/*: RecyclerView.Adapter<RecyclerView.ViewHolder>() */{
+/*
+    private var items: List<Pedido> = ArrayList()
+
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+/*
+    class InicioViewHolder constructor(
+        itemView: View
+    ) : RecyclerView.ViewHolder(itemView) {
+/*
+        val imagen = itemView.img_receta
+        val nombreLocal = itemView.et_nombre_local
+        val precio = itemView.et_precio
+        val tiempo = itemView.et_tiempo
+        val puntuacion = itemView.et_puntuacion
+
+*/
+
+
+    }
+*/
+
+ */
+}
