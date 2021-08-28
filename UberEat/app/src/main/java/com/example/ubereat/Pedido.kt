433a3a80d88620cb45ab212ef8a1ce8a5ccc389e
nuevo <nuevo>
2021-08-28 15:45:08 -0500

    adapter inicio

--- UberEat/app/src/main/java/com/example/ubereat/Pedido.kt
+++ UberEat/app/src/main/java/com/example/ubereat/Pedido.kt
@@ -1,6 +1,7 @@
 package com.example.ubereat
 
 class Pedido(
+    val imagenPedido: Int,
     val nombreLocal: String,
     val costoEnvio: Double,
     val tiempo: String,
--- UberEat/app/src/main/java/com/example/ubereat/adapters/InicioAdapter.kt
+++ UberEat/app/src/main/java/com/example/ubereat/adapters/InicioAdapter.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.PopupMenu
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -12,14 +14,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.ubereat.Pedido
 import com.example.ubereat.R
 
-class InicioAdapter/*: RecyclerView.Adapter<RecyclerView.ViewHolder>() */{
-/*
+class InicioAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private var items: List<Pedido> = ArrayList()
 
-
-
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+        when(holder)
+        {
+            is InicioViewHolder -> {
+                holder.bind(items.get(position))
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -27,24 +32,37 @@ class InicioAdapter/*: RecyclerView.Adapter<RecyclerView.ViewHolder>() */{
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
+        return InicioViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.layout_inicio_recycler,
+                parent,
+                false
+            )/*, mListener*/
+        )
     }
-/*
+
     class InicioViewHolder constructor(
-        itemView: View
-    ) : RecyclerView.ViewHolder(itemView) {
-/*
-        val imagen = itemView.img_receta
-        val nombreLocal = itemView.et_nombre_local
-        val precio = itemView.et_precio
-        val tiempo = itemView.et_tiempo
-        val puntuacion = itemView.et_puntuacion
+        view: View
+    ) : RecyclerView.ViewHolder(view) {
 
-*/
+        val imagen = view.findViewById<ImageView>(R.id.img_receta)
+        val nombreLocal = view.findViewById<TextView>(R.id.et_nombre_local)
+        val precio = view.findViewById<TextView>(R.id.et_precio)
+        val tiempo = view.findViewById<TextView>(R.id.et_tiempo)
+        val puntuacion = view.findViewById<TextView>(R.id.et_puntuacion)
+
+
+        fun bind(pedido: Pedido) {
+            imagen.setImageResource(pedido.imagenPedido)
+            nombreLocal.setText(pedido.nombreLocal)
+            precio.setText(pedido.costoEnvio.toString())
+            tiempo.setText(pedido.tiempo)
+            puntuacion.setText(pedido.puntuacion.toString())
+        }
 
 
     }
-*/
 
- */
+
+
 }
